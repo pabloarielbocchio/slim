3786adf9d12b5cba235998d412cee0e19f3fd4b1
pabloarielbocchio <pablobocchio@gmail.com>
2020-12-23 18:20:21 -0300

    primera
    prueba

---
+++ restapi/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'restapi');
+
+define('API_KEY','3d524a53c110e4c22463b10ed32cef9d');
+
+?>
